49025fb760c1d54e5cf9e62e4fda516bd9d68dfe
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-12 22:25:03 +0530

    commit

---
+++ ch4/05_tuplemethod.py
@@ -0,0 +1,17 @@
+a=(1,3,678,3,"jats","rats")
+print(a)
+
+no=a.count(3)
+print(no)
+no=a.index(1)
+print(no)
+print(a.index("jats"))
+print(a.index(3))
+
+tup=(1,2,3,4)
+rep=tup*3
+print(rep)
+print(2 in tup)
+print(6 in tup)
+
+#more methods search on chatgpt
