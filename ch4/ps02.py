dbb6a0911063b668a0bd9a463c64a36d74889953
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-12 22:52:47 +0530

    commit

---
+++ ch4/ps02.py
@@ -0,0 +1,18 @@
+marks=[]
+
+
+f1=input("enter marks name:")
+marks.append(f1)
+f2=input("enter marks name:")
+marks.append(f2)
+f5=input("enter marks name:")
+marks.append(f5)
+f3=input("enter marks name:")
+marks.append(f3)
+f4=input("enter marks name:")
+marks.append(f4)
+
+print(marks)
+
+marks.sort()
+print(marks)
