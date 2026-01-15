ca39b58bc0c654238942b96b44b1e0e76b81d296
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-15 15:02:40 +0530

    commit

---
+++ ch5/01dict.py
@@ -0,0 +1,14 @@
+marks={
+    "ritika":100,
+    "arya":98,       #all indexing has been done
+    "jats":99
+}
+marks={"ritika":98}      #dict is mutable 
+
+print(marks,type(marks))
+#print(marks[0]) error
+print(marks["ritika"])
+
+"""we can also form lists in lists for eg
+marks=[["harry",100]]
+it wont give 100 as output of harry"""
