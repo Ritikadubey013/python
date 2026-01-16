231e46dbc73452d892223b4cbbb281bd90599927
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-16 17:56:19 +0530

    commit

---
+++ ch5/02dict.method.py
@@ -0,0 +1,24 @@
+marks={
+    "ritika":100,
+    "arya":98,       
+    "jats":99
+}
+
+print(marks.items())
+print(marks.keys())
+print(marks.values())
+marks.update({"ritika":90,"xyz":98})
+print(marks)
+print(marks.get("ritika"))
+print(marks.get("ritika2")) #prints none
+print(marks["ritika2"]) #returns an error
+
+
+
+
+
+
+
+
+
+#chatgpt more methods 
