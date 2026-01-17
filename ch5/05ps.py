8a40e82de01da7b295b0f045009cd523cff57108
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-17 17:25:29 +0530

    commit

--- ch5/05ps.py
+++ ch5/05ps.py
@@ -15,7 +15,7 @@ lang=input("enter lang name:")
 
 d.update({name:lang})
 
-name=input("enter friends name")
+name=input("enter friends name:")
 lang=input("enter lang name:")
 
 d.update({name:lang})
