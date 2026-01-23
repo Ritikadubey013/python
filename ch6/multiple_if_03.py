9fc19487d3cff3b504b6bb037bf158fe5d832d8c
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-23 22:40:38 +0530

    commit

---
+++ ch6/multiple_if_03.py
@@ -0,0 +1,19 @@
+a=int(input("enter your age"))
+
+#if statement 1
+if(a%2==0):
+    print("a is even ")
+
+#if statement 2
+if(a>=18):
+    print("yes")
+
+elif(a==0):
+    print("wrong")
+
+elif(a<0):
+    print("very wrong")
+
+else:
+    print("no")
+ 
