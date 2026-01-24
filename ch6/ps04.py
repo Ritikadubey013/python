7a800f2a41f7fcf1bfa9a04711560b5a963c5cb5
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-24 22:56:43 +0530

    commit

---
+++ ch6/ps04.py
@@ -0,0 +1,7 @@
+username=input("enter username")
+
+if(len(username))<10:
+    print("your username contains less than 10 charac")
+
+else:
+    print("your username containse more than or equal to 10 character")
