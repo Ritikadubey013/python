a3a7a33d2a698b7e8d2434b9aa6426545701d2d8
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-24 22:11:10 +0530

    commit

--- ch6/ps02.py
+++ ch6/ps02.py
@@ -6,7 +6,7 @@ sub3=int(input("enter cse marks:"))
 
 total_percentage= (100*(sub1+sub2+sub3))/300
 
-if(total_percentage>=40 and sub1>33 and sub2>33 and sub3>33):
+if(total_percentage>=40 and sub1>=33 and sub2>=33 and sub3>=33):
     print("you are pass",total_percentage)
 
 else:
