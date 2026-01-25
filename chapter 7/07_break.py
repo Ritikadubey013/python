191e074ef86f3ccb57d7bf45115d49520ac3d811
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-25 15:33:55 +0530

    commit

---
+++ chapter 7/07_break.py
@@ -0,0 +1,9 @@
+for i in range (100):
+    if(i==56):
+        break #exit the loop right now 
+    print(i)
+
+for l in range(100):
+    if(l==56): # skips this iteration 
+        continue
+    print(l)
---
+++ chapter 7/08_pass.py
@@ -0,0 +1,8 @@
+for i in range(456):
+    pass
+
+
+i=0
+while(i<10):
+    print(i)
+    i+=1
