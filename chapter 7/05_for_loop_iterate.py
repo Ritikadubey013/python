84b1eedc331cc816af1382c96c1518195f810b0d
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-25 15:11:34 +0530

    commit

---
+++ chapter 7/05_for_loop_iterate.py
@@ -0,0 +1,14 @@
+#for loops with lists
+l=[1,4,6,"harry"]
+for i in l:
+    print(i)
+
+#for loops with tuples
+t={6,345,23,89}
+for i in t:
+    print(i)
+
+#for loops with string
+s="ritika"
+for i in s:
+    print(i)
