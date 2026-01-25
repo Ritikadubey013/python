f5a6d2f425765a7879e05ff68405246e8538ee68
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-25 13:08:58 +0530

    commit

---
+++ chapter 7/02whileloop.py
@@ -0,0 +1,19 @@
+i =1
+
+while(i<20):
+    print(i)
+    i+=3 # this condition will be checked always 
+    #that goes into while statement and prints the output
+    #till the statement is true
+
+i=0
+
+while i<5:
+    print("harry")
+    i=i+1
+
+i=0
+
+while i<51:
+    print(i)
+    i+=1
---
+++ chapter 7/03_list_using_while.py
@@ -0,0 +1,10 @@
+l=[1,"harry",False,"this","those"]
+
+i=0
+
+while(i<len(l)):
+    print(l[i])
+    i+=1
+
+for l in range(1,6):
+    print(l)
