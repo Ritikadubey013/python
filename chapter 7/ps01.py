1c28cdbd61ade87decb30e65f3c4b1b87458a7f2
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-25 15:50:33 +0530

    commit

---
+++ chapter 7/ps01.py
@@ -0,0 +1,10 @@
+n=int(input("enter the num:"))
+
+for i in range(1,11):
+    print(f"{n} X {i}={n*i}")
+
+
+n=int(input("enter your desried num"))
+
+for i in range(1,11):
+    print(f"{n}X{i}={n*i}")
