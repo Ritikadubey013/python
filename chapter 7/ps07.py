1a40d950eec73bba1aa7e05d1835ed7f1fca80b0
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-26 21:27:13 +0530

    commit

--- chapter 7/ps07.py
+++ chapter 7/ps07.py
@@ -10,4 +10,4 @@ n=int(input("enter your no:"))
 for i in range(1,n+1):
     print(" "*( n-i),end="") #this end statement wont add default line 
     print("*" * (2*i-1), end="")
-    print("\n")
+    print(" ")
