6310191b69815a7e0af9c10e1226d35e363052c9
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-28 23:42:06 +0530

    commit

---
+++ chapter8/04.funct.py
@@ -0,0 +1,7 @@
+def goodDay(name,ending):#function definition
+    print("good day,"+name)
+    print(ending)
+    return "oki?"
+
+a=goodDay("ritika","proud of you")#function call
+print(a)  #here the value written in return will come
