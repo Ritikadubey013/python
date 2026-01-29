9ec0beea5b03581cc42a5bcb46723798e528c67c
Ritika dubey <ritikadubey1305@gmail.com>
2026-01-29 22:50:57 +0530

    commit

--- chapter8/05.funct.py
+++ chapter8/05.funct.py
@@ -2,3 +2,10 @@ def hello(name ="stranger"):
     print(name)
 hello()
 hello("ritika")
+
+def goodDay(name, ending="thank you"):
+    print(f"good day, {name}")
+    print(ending)
+
+goodDay("ritika")
+goodDay("ritika","I'm proud of you")
